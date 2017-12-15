fee4e49d3e1db29e5ffa3413ad3483b1fd685453
Daniel Cook <pawc3@cox.net>
2017-12-15 00:16:42 -0600

    Add files via upload

---
+++ Vive.bat
@@ -0,0 +1,5 @@
+nircmd setdefaultsounddevice "Speakers" 1
+nircmd setdefaultsounddevice "Speakers" 2
+nircmd setdefaultsounddevice "Microphone" 1
+nircmd setdefaultsounddevice "Microphone" 2
+taskkill /f /im Discord.exe
