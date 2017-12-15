bad0d391fde09f8fc9fa7a92fce140c26bab6019
Daniel Cook <pawc3@cox.net>
2017-12-15 00:13:14 -0600

    Add files via upload

---
+++ Headphones.bat
@@ -0,0 +1,6 @@
+nircmd setdefaultsounddevice "Digital Audio (S/PDIF)" 1
+nircmd setdefaultsounddevice "Digital Audio (S/PDIF)" 2
+nircmd setdefaultsounddevice "Line" 1
+nircmd setdefaultsounddevice "Line" 2
+taskkill /f /im Discord.exe
+@pause 
